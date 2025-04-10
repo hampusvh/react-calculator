faff94cb97facc728c82bb8d8f6120a9ba7484dd
hampusvh <hampus.vh@gmail.com>
2025-04-10 13:00:46 +0200

    general styling and layout of elements

--- react-calculator/src/components/Button.jsx
+++ react-calculator/src/components/Button.jsx
@@ -1,3 +1,5 @@
+import "../App.css";
+
 function Button({ value }) {
   return <button>{value}</button>;
 }
--- react-calculator/src/components/Display.jsx
+++ react-calculator/src/components/Display.jsx
@@ -1,3 +1,5 @@
+import "../App.css";
+
 function Display() {
   return <div className="display">123</div>;
 }
